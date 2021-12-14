4e4a70944c86e7532f68520e1f02089420c60a9d
Elias <ruizelias409@gmail.com>
2021-12-14 08:34:51 +0100

    first commit

---
+++ src/main/java/com/emi/app/App.java
@@ -0,0 +1,13 @@
+package com.emi.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
